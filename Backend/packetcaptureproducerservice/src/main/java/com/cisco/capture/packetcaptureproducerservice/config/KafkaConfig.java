2b410c2453f2d71bb87909404f69e664a9149e96
santrajrakesh <rakesh.santraj4@gmail.com>
2025-09-24 12:29:38 +0530

    committing code for replication factor

--- Backend/packetcaptureproducerservice/src/main/java/com/cisco/capture/packetcaptureproducerservice/config/KafkaConfig.java
+++ Backend/packetcaptureproducerservice/src/main/java/com/cisco/capture/packetcaptureproducerservice/config/KafkaConfig.java
@@ -1,6 +1,7 @@
 package com.cisco.capture.packetcaptureproducerservice.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -8,12 +9,15 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaConfig {
 
+	@Value("${spring.kafka.admin.properties.replication.factor}")
+	private int replicationFactor;
+	
 	@Bean
 	public NewTopic packetTopic() {
 		return TopicBuilder
 				.name("packets1")
 				.partitions(3)
-				.replicas(3)
+				.replicas(replicationFactor)
 				.build();
 	}
 }
